66062210da93015587c00d96bd858ee628d17d8a
CheruBea <bettybellaann@gmail.com>
2024-11-01 20:50:20 +0300

    files and notes functionalities

--- CarpCon/CarpCon/urls.py
+++ CarpCon/CarpCon/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,4 +9,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('', include('account.urls')),
     path('projects/', include('project.urls')),
-]
+    path('projects/<uuid:project_id>/', include('todolist.urls')),
+    path('projects/<uuid:project_id>/<uuid:todolist_id>/', include('task.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
